d9b9fa82ec6b9b1affdb286f99b3f7ee13bd4206
Joseph Tang <jtang@cardinal-rw01.ten.osc.edu>
2025-11-13 19:10:48 -0500

    fix comment typo

--- run_full_AF2_pipeline.sh
+++ run_full_AF2_pipeline.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# USAGE: ./run_full_af2_pipeline.sh {protein_id}
+# Run AF2 pipeline entering protein_id;
+# USAGE: ./run_full_AF2_pipeline.sh $PROTEIN_ID
 
 PROTEIN_ID=$1
-WORKING_DIR=$2
+WORKING_DIR=$2 
 
 # DIRECTORIES
 BASE_DIR="$WORKING_DIR/AF2_outputs"       # Where AF2 outputs are saved
@@ -17,10 +18,10 @@ python3 fastas.py -p $PROTEIN_ID -o $FASTA_DIR -d $DATABASE
 
 # wait for fastas file being created:
 EXPECTED_FASTA="${FASTA_DIR}/${PROTEIN_ID}.fasta"
-echo "[2b/4] Waiting for fasta file to be created"
+echo "[1b/4] Waiting for fasta file to be created"
 while [ -z "$(ls $EXPECTED_FASTA 2>/dev/null)" ]; do
     sleep 10
-    echo "waiting for fasta files"
+    echo "waiting for fasta file"
 done
 echo "fasta file found, continuing."
 
@@ -30,10 +31,10 @@ python3 AF_full_run.py $PROTEIN_ID
 
 # Wait for pickle, pdb, and JSON files are created.
 EXPECTED_PDB="${BASE_DIR}/${PROTEIN_ID}/relaxed_model_*.pdb"
-echo "[2b/4] Waiting for relaxed PDB to be created..."
+echo "[2b/4] Waiting for relaxed PDB to be created"
 while [ -z "$(ls $EXPECTED_PDB 2>/dev/null)" ]; do
     sleep 60
-    echo "still waiting for output files"
+    echo "waiting for output files"
 done
 echo "PDB file found, continuing."
 
@@ -45,7 +46,8 @@ python3 run_analysis.py \
   --output_dir $OUTPUT_DIR \
 
 # Generate pLDDT/PAE Plots & Launch PyMOL
-echo "[4/4] Visualizing with pLDDT and PyMOL"
+echo "[4/4] Visualizing with pLDDT, PAE, and PyMOL"
 python3 pLDDT_PAE_plot.py -i ${OUTPUT_DIR}/${PROTEIN_ID}.pkl -o ${OUTPUT_DIR}
 pymol ${OUTPUT_DIR}/${PROTEIN_ID}.pdb
+
 echo "Pipeline finished for $PROTEIN_ID"
