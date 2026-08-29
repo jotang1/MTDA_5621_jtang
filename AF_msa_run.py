888445d2785dab7d24945c7fd99aaffef1b80868
Joseph Tang <jtang@cardinal-rw02.ten.osc.edu>
2026-08-29 04:14:11 -0400

    update container and path

--- AF_msa_run.py
+++ AF_msa_run.py
@@ -5,12 +5,12 @@ import sys
 
 protein_id = sys.argv[1]
 
-dir="{working_dir}"
+dir="/path/to/working/dir"
 
 command = f"""
-{dir}/AF_msa_SI2025.sh \
-  --fasta_paths={dir}/fastas/{protein_id}.fasta \
-  --output_dir={dir}/msas/{output_dir} \
+{dir}/scripts/AF_container_wrapped.sh \
+  --fasta_paths={dir}/AF2_fastas/{protein_id}.fasta \
+  --output_dir={dir}/AF2_outputs \
   --model_preset=monomer_ptm \
   --max_template_date=2023-01-01 \
   --db_preset=reduced_dbs \
