b752378ffe2b4495a1057891eaf957a557f160d0
Joseph Tang <jtang@cardinal-rw02.ten.osc.edu>
2026-09-02 16:49:03 -0400

    update workflow paths and usage instructions

--- run_full_AF2_pipeline_custom.sh
+++ run_full_AF2_pipeline_custom.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# USAGE: ./run_full_AF2_pipeline.sh $PROTEIN_ID
+# With a customized fasta file and protein_id
+# USAGE: ./run_full_AF2_pipeline_custom.sh $PROTEIN_ID
 
 PROTEIN_ID=$1
 WORKING_DIR="/path/to/working/dir"
 
 # DIRECTORIES
-BASE_DIR="$WORKING_DIR/AF2_outputs"       # AF2 outputs saved
-OUTPUT_DIR="$WORKING_DIR/AF2_plots"       # renamed .pdb/.pkl
-FASTA_DIR="$WORKING_DIR/AF2_fastas"       # input FASTA files
-DATABASE="/fs/scratch/PZS1154/alphafold/uniprot_all_2021_04.fa"
+BASE_DIR="$WORKING_DIR/AF2_outputs"             # AF2 outputs (msas dir, pkl, pdb, and json files)
+OUTPUT_DIR="$WORKING_DIR/AF2_plots"             # renamed .pdb/.pkl, pLDDT and PAE plots
+FASTA_DIR="$WORKING_DIR/AF2_fastas"             # input FASTA files
+DATABASE="/$WORKING_DIR/uniprot_all_2021_04.fa"
 
+# Look for the fastas file:
 EXPECTED_FASTA="${FASTA_DIR}/${PROTEIN_ID}.fasta"
+echo "[1/4] Waiting for fasta file to be created"
+while [ -z "$(ls $EXPECTED_FASTA 2>/dev/null)" ]; do
+    sleep 10
+    echo "waiting for fasta file"
+done
+echo "fasta file found, continuing."
 
 # Run AlphaFold2
 echo "[2/4] Submitting AF2 job for $PROTEIN_ID"
